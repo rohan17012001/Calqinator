7044d59cc21324601f8a7a8eea2e2a45d0f5f7d1
Rohan <75871824+rohan17012001@users.noreply.github.com>
2022-07-21 14:51:34 +0530

    updated script.js for invalid operations

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function enter_val(val){
-    document.getElementById("values").value+=val;
-  }
-function bkspce(){  
-    document.getElementById("values").value=document.getElementById("values").value.slice(0,-1);
-  }
-  function clrscr(){
+  document.getElementById("values").value+=val;
+}
+function bkspce(){  document.getElementById("values").value=document.getElementById("values").value.slice(0,-1);
+}
+function clrscr(){
+  document.getElementById("values").value="";
+}
+function solve(){
+  let y=document.getElementById("values").value;
+  let x=eval(y);
+  if(x=="Infinity"){
+    alert("Invalid operation! \nDivision by zero is not defined!");
     document.getElementById("values").value="";
   }
-  function solve(){
-    let y=document.getElementById("values").value;
-    let x=eval(y);
-    document.getElementById("values").value=x;
+  else{
+    var n=x.toFixed(7);
+  document.getElementById("values").value=n;
   }
+}
